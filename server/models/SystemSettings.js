8915054a8c71c2e69b36ba2681fbae0448d3373d
AJAYLAPTOP\HIG Ajay <msuji012001@gmail.com>
2026-07-13 16:55:20 +0530

    fixed

---
+++ server/models/SystemSettings.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const systemSettingsSchema = new mongoose.Schema({
+  // Only one settings document should ever exist, but we use a key to ensure uniqueness
+  key: {
+    type: String,
+    default: 'global',
+    unique: true
+  },
+  stageColors: {
+    type: Map,
+    of: String,
+    default: {
+      'Booking': '#0a4c2c', // The dark green default
+    }
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('SystemSettings', systemSettingsSchema);
